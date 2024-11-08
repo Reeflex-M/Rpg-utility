4726a0efde0fce70d97ec92915e2826e4bb8d930
Mathis <mathisfloch29@gmx.fr>
2024-11-08 23:27:04 +0100

    y

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Rpg-utility/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/Rpg-utility/' // Doit correspondre au nom de votre dépôt
 });
